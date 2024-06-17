949623239ba77783a7f1e45159c9850782a2dfd8
jay prajapati <79649559+jayy-77@users.noreply.github.com>
2024-06-17 22:11:30 +0530

    v1.2 -> Reading pipeline from yml

--- src/main/java/com/vecops/Config.java
+++ src/main/java/com/vecops/Config.java
@@ -1,5 +1,11 @@
 package com.vecops;
+
 import java.util.List;
+
+class Pipeline {
+    public String name;
+    public int port;
+}
 
 class Source {
     public String name;
@@ -22,6 +28,7 @@ class Webhook {
 }
 
 class Config {
+    public List<Pipeline> pipelines;
     public List<Source> sources;
     public List<Sink> sinks;
     public Service service;
